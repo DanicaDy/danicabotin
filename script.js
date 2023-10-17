439012484ef66ffa51edc85c2875ec242825fed1
DanicaDy <147595425+DanicaDy@users.noreply.github.com>
2023-10-17 23:43:19 +0800

    final

---
+++ script.js
@@ -0,0 +1,57 @@
+// active hamburger menu 
+let menuIcon = document.querySelector(".menu-icon");
+let navlist = document.querySelector(".navlist")
+menuIcon.addEventListener("click",()=>{
+    menuIcon.classList.toggle("active");
+    navlist.classList.toggle("active");
+    document.body.classList.toggle("open");
+});
+
+// remove navlist
+navlist.addEventListener("click",()=>{
+    navlist.classList.remove("active");
+    menuIcon.classList.remove("active");
+    document.body.classList.remove("open");
+})
+
+// switch between about buttons 
+
+const buttons = document.querySelectorAll('.about-btn button');
+const contents = document.querySelectorAll('.content');
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    contents.forEach(content => content.style.display = 'none');
+    contents[index].style.display = 'block';
+    buttons.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+  });
+});
+
+
+
+// portfolio fillter 
+
+var mixer = mixitup('.portfolio-gallery',{
+    selectors: {
+        target: '.portfolio-box'
+    },
+    animation: {
+        duration: 500
+    }
+});
+
+// active menu 
+
+let menuLi = document.querySelectorAll("header ul li a");
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
